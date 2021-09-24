327078cc1b911901fc007e43eaf2c93aae77dd3c
msr <mshineray@qq.com>
2021-09-24 11:02:18 +0800

    upd: test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="app">模版--基于vue、webpack构建umd组件</div>
+  <div class="app">模版--基于vue、webpack构建umd组件 {{ testData }}</div>
 </template>
 
 <script>
 export default {
   components: {},
+  props: {
+    testData: {
+      type: String,
+      default: '',
+    },
+  },
   mixins: [],
   data() {
     return {}
